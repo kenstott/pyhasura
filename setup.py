433903480f4d092bdb0303a074a39376cef4c06f
kenstott <128912107+kenstott@users.noreply.github.com>
2024-04-20 08:32:17 -0400

    initial commit

--- setup.py
+++ setup.py
@@ -5,9 +5,14 @@ from pathlib import Path
 this_directory = Path(__file__).parent
 long_description = (this_directory / "README.md").read_text()
 
+version = {}
+with open("./__version__.py") as fp:
+    exec(fp.read(), version)
+
 setup(
     name='pyhasura',
     # other arguments omitted
     long_description=long_description,
     long_description_content_type='text/markdown'
 )
+
